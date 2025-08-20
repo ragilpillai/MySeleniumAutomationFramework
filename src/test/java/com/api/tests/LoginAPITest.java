706a7fb1ba737d54897a847ee7d30df9cad9c044
Ragil <ragil.pillai@gmail.com>
2025-08-20 21:12:16 +0530

    Commit Login API File

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -3,7 +3,8 @@ package com.api.tests;
 public class LoginAPITest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Login API Test:");
 
 	}
 
